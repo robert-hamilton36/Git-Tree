31fa304ad3153c6038e33cc493c2822094be1dc2
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-08-28 16:38:24 +1200

    add source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: "source-map",
   entry: './src/content-script.tsx',
   module: {
     rules: [
