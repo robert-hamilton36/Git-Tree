c52a35a0e6f734bfbdd477ba870070be8011811c
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-08-28 16:43:35 +1200

    fix prop formatting

--- src/svgs/FolderSVG.tsx
+++ src/svgs/FolderSVG.tsx
@@ -5,8 +5,10 @@ export const FolderSVG = () => {
     <svg 
       aria-label="Directory" 
       aria-hidden="true" 
-      height="16" viewBox="0 0 16 16" 
-      version="1.1" width="16" 
+      height="16" 
+      viewBox="0 0 16 16" 
+      version="1.1" 
+      width="16" 
       data-view-component="true" 
       className="octicon octicon-file-directory-fill hx_color-icon-directory"
     >
