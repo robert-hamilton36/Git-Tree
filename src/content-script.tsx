66aa973f9f4424239c6484e762c8baa3c86afeaa
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-09-01 19:44:14 +1200

    fix popstate crash

--- src/content-script.tsx
+++ src/content-script.tsx
@@ -1,19 +1,48 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App"
-import { addGitPageContainer, createSidebarNav } from "./util/createElements";
+import { addGitPageContainer, addSidebarNav } from "./util/createElements";
 
-// add flex
+/**
+ * 1 set body to be flex
+ * 2 add container around githubs content, the only child of document.body
+ * 3 inserts the side nav bar before the above container
+ */
+
 document.body.style.display = 'flex'
-
-// containerise the github content
 addGitPageContainer()
-//creates sidebar
-createSidebarNav()
+addSidebarNav()
 
+/**
+* popstate happens whenever the back or forwards button is pressed
+* when this happens the react app looses reactivity
+* so these listeners fix that
+*/
+
+// removes react app on back of forward button press
+window.addEventListener('popstate', () => {
+  root.unmount()
+})
+
+
+// event is called whenever data is rendered by turbo
+// twice when data has been cached (loading cached data/ refreshing data)
+// this checks for react app, if it doesn't exist render it again
+document.addEventListener('turbo:render', () => {
+  const getNav = document.getElementById('GitTree-nav')
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  if (!root._internalRoot) {
+    root = createRoot(getNav)
+    root.render(<App />)
+  }
+
+})
+
+
+// first render of react app
 const getNav = document.getElementById('GitTree-nav')
-
-const root = createRoot(getNav)
+let root = createRoot(getNav)
 
 root.render(
   <App/>
