022ca6126cef13ebf063c657a6282e8f69ea458c
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-09-06 15:00:03 +1200

    add test

---
+++ src/contexts/__tests__/GitRepoContext.test.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { renderHook, act } from '@testing-library/react'
+import { GitRepoProvider, useGitRepo } from "../GitRepoContexts"
+
+describe('<GitRepoProvider>', () => {
+  test('renders provider with initial state', () => {
+    const wrapper = ({ children }: ReactChildren) => <GitRepoProvider> {children} </GitRepoProvider>
+    const { result } = renderHook(() => useGitRepo(), { wrapper })
+
+    expect(result.current.user).toBe('')
+    expect(result.current.repo).toBe('')
+    expect(result.current.branch).toBe('')
+  })
+
+  test('sets user, repo and branch', () => {
+    const wrapper = ({ children }: ReactChildren) => <GitRepoProvider> {children} </GitRepoProvider>
+    const { result } = renderHook(() => useGitRepo(), { wrapper })
+
+    expect(result.current.user).toBe('')
+    expect(result.current.repo).toBe('')
+    expect(result.current.branch).toBe('')
+
+    act(() => result.current.setUser('robert-hamilton36'))
+    act(() => result.current.setRepo('GitTree'))
+    act(() => result.current.setBranch('main'))
+
+    expect(result.current.user).toBe('robert-hamilton36')
+    expect(result.current.repo).toBe('GitTree')
+    expect(result.current.branch).toBe('main')
+  })
+})
