539fcd929e9b6b4d17507a50bf1a79ae472b122e
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-09-08 11:48:02 +1200

    add gitrepo

--- src/contexts/__tests__/Provider.test.tsx
+++ src/contexts/__tests__/Provider.test.tsx
@@ -3,18 +3,29 @@ import { render, renderHook } from '@testing-library/react'
 import { Providers } from '../Providers'
 import { useNavWidth } from '../NavWidthContext'
 import { useTree } from '../TreeContext'
+import { useGitRepo } from '../GitRepoContexts'
 
 describe('<Providers>', () => {
   test('renders all Providers and has access to all contexts', () => {
     const wrapper = ({ children }: ReactChildren) => <Providers> {children} </Providers>
     const { result: navResult } = renderHook(() => useNavWidth(), { wrapper })
     const { result: treeResult } = renderHook(() => useTree(), { wrapper })
+    const { result: gitRepoResult } = renderHook(() => useGitRepo(), { wrapper })
 
     expect(navResult.current.navWidth).toBe(201)
     expect(navResult.current.setNavWidth).toBeTruthy()
 
     expect(treeResult.current.tree).toEqual([])
     expect(treeResult.current.setTree).toBeTruthy()
+
+    expect(gitRepoResult.current.user).toEqual('')
+    expect(gitRepoResult.current.setUser).toBeTruthy()
+
+    expect(gitRepoResult.current.repo).toEqual('')
+    expect(gitRepoResult.current.setRepo).toBeTruthy()
+
+    expect(gitRepoResult.current.branch).toEqual('')
+    expect(gitRepoResult.current.setBranch).toBeTruthy()
   })
   test('renders children', () => {
     const Children = () => {
