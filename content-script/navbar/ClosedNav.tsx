df08cd31434f4274ce811093e6edc8f924ab6332
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-09-19 09:41:01 +1200

    add ul

--- content-script/navbar/ClosedNav.tsx
+++ content-script/navbar/ClosedNav.tsx
@@ -1,29 +1,42 @@
 import React from 'react'
 import { ExplorerSVG } from '../svgs/ExplorerSVG'
+import { UserSVG } from '../svgs/UserSVG'
 
 /*
   css classes --color-page-header-bg && btn-link color-fg-muted supplied by github
 */
 
-export const ClosedNav: React.FC<Props> = ({setShowNav}) => {
+export const ClosedNav: React.FC<Props> = ({setShowNav, setView}) => {
 
-  const handleClick = () => {
+  const handleClick = (view: PageView) => {
     setShowNav(state => !state)
+    setView(view)
   }
   return (
     <div
       className='--color-page-header-bg GitTree-closedNavigator'
     >
-      <button
-        className="btn-link color-fg-muted"
-        onClick={handleClick}
-      >
-        <ExplorerSVG />
-      </button>
+      <ul>
+        <li
+          className="btn-link color-fg-muted gitTree-actionItem"
+          onClick={() => handleClick('tree')}
+        >
+          <ExplorerSVG />
+        </li>
+      </ul>
+      <ul>
+        <li
+          className="btn-link color-fg-muted gitTree-actionItem"
+          onClick={() => handleClick('user')}
+        >
+          <UserSVG />
+        </li>
+      </ul>
     </div>
   )
 }
 
 interface Props {
   setShowNav: React.Dispatch<React.SetStateAction<boolean>>;
+  setView: React.Dispatch<React.SetStateAction<PageView>>;
 }
