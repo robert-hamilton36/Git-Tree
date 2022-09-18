e3817afbe023b27eba40906772755b4998028790
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-09-19 10:56:28 +1200

    remove svg

--- content-script/navbar/Header.tsx
+++ content-script/navbar/Header.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
 
-import { ExplorerSVG } from '../svgs/ExplorerSVG'
 
-interface Props {
-  setShowNav: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-export const Header: React.FC<Props> = ({ setShowNav }) => {
-
-  const handleClick = () => {
-    setShowNav((state) => !state)
-  }
-
+export const Header: React.FC = () => {
   return (
     <header className='GitTree-header'>
-      <button
-        // css class supplied by github
-        className="btn-link color-fg-muted"
-        onClick={handleClick}
-      >
-        <ExplorerSVG />
-      </button>
+
       {/* css class supplied by github */}
       <h1 className='Header-link Header'>
         Git Tree
--- content-script/navbar/OpenNav.tsx
+++ content-script/navbar/OpenNav.tsx
@@ -2,20 +2,19 @@ import React from 'react'
 import { useNavWidth } from '../contexts/NavWidthContext'
 import { Header } from './Header'
 
-export const OpenNav: React.FC<Props> = ({setShowNav, content}) => {
+export const OpenNav: React.FC<Props> = ({content}) => {
   const { navWidth } = useNavWidth()
   return (
     <div
       style={{width: navWidth - 1}}
       className='--color-page-header-bg GitTree-openNavigator'
     >
-      <Header setShowNav={setShowNav} />
+      <Header />
       {content}
     </div>
   )
 }
 
 interface Props {
-  setShowNav: React.Dispatch<React.SetStateAction<boolean>>;
   content: React.ReactNode;
 }
