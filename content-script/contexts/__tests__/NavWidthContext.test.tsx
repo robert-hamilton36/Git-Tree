625ae09c2a5cf17b62001cb9e1c6d59465b2c5fd
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-10-05 12:48:30 +1300

    update tests

--- content-script/contexts/__tests__/NavWidthContext.test.tsx
+++ content-script/contexts/__tests__/NavWidthContext.test.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { renderHook, act } from '@testing-library/react'
 import { NavWidthProvider, TestStartingWidth, useNavWidth } from '../NavWidthContext'
+import { editDocumentBodyMargin } from '../../util/createElements'
+
+jest.mock('../../util/createElements')
+
+const MockEditDocumentBodyMargin = editDocumentBodyMargin as jest.Mock
+
 
 describe('<NavWidthProvider>', () => {
   test('renders provider with initial state', () => {
@@ -15,10 +21,13 @@ describe('<NavWidthProvider>', () => {
     const { result } = renderHook(() => useNavWidth(), { wrapper })
 
     expect(result.current.navWidth).toBe(TestStartingWidth)
+    expect(MockEditDocumentBodyMargin).toHaveBeenCalledTimes(0)
+
 
     act(() => result.current.setNavWidth(400))
 
     expect(result.current.navWidth).toBe(400)
+    expect(MockEditDocumentBodyMargin).toHaveBeenCalledTimes(1)
   })
 })
 
